ce7e34fa810282c9c261286fac0faf2d20fb22db
wort1k <wort1k17@gmail.com>
2024-11-04 16:13:53 +0300

    Merge branch 'BE' of https://github.com/AnnDets/Find-your-pet into BE
    # Conflicts:
    #	pom.xml
    #	src/main/java/DBControllers/DBController.java
    #	target/classes/DBControllers/DBController.class

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import models.User;
+
+import java.util.ArrayList;
+
+public interface DAO {
+    boolean create(Object object);
+    boolean update(Object object);
+    boolean delete(Object object);
+    Object read(Object object);
+    ArrayList<User> readAll();
+
+}
