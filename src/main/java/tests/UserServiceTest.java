750a09e62585da752c53f49bea2ecf7afe077ae4
HONOR <sofikulich@icloud.com>
2024-11-08 16:10:58 +0300

    изменение тестов для UserService

--- src/main/java/tests/UserServiceTest.java
+++ src/main/java/tests/UserServiceTest.java
@@ -4,11 +4,15 @@ import DBControllers.DBController;
 import models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import services.ReportController;
 import services.UserService;
 import utils.*;
 
+import dao.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -21,116 +25,100 @@ class UserServiceTest {
     @BeforeEach
     void setUp() {
         dbController = mock(DBController.class);
-        userService = new UserService(dbController);
+        userService = new UserService(ReportController);
     }
 
     //Проверяет, что при вводе корректных данных функция addUser возвращает пустой список ошибок.
     @Test
-    void addUser_ValidData_ShouldReturnEmptyErrors() {
+    void testAddUserValidData() {
         // Arrange
-        String name = "Test User";
-        String email = "test23@example.com";
-        String phone = "1234567890";
-        String plainPassword = "Pewrvrvg#d135";
-        String address = "Test Address";
+        User user= new User(1,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");
 
-        //when(dbController.addUserToDB(name, email, phone, anyString(), address)).thenReturn(true);
+        when(UserDao.create(user)).thenReturn(true);
 
-        // Act
-        ArrayList<AddUserError> errors = userService.addUser(
-                name, email, phone, plainPassword, address);
+        ArrayList<AddUserError> errors = userService.addUser(user);
 
-        // Assert
         assertTrue(errors.isEmpty());
-        verify(dbController).addUserToDB(name, email, phone, anyString(), address);
+        verify(UserDao, times(1)).create(user);
+
     }
 
     //Проверяет, что при вводе некорректного адреса электронной почты функция addUser возвращает ошибку INVALID_EMAIL.
     @Test
-    void addUser_InvalidEmail_ShouldReturnInvalidEmailError() {
-        // Arrange
-        String name = "Test User";
-        String email = "invalid.email";
-        String phone = "1234567890";
-        String plainPassword = "Password123";
-        String address = "Test Address";
+    void testAddUserInvalidEmail() {
+        User user= new User(2,"Test User","invalid-email",
+                "1234567890", "Pewrvrvg#d135","Test Address");
 
         // Act
-        ArrayList<AddUserError> errors = userService.addUser(name, email, phone, plainPassword, address);
+        ArrayList<AddUserError> errors = userService.addUser(user);
 
-        // Assert
         //assertEquals(1, errors.size());
         assertTrue(errors.contains(PhoneAndEmailError.INVALID_EMAIL));
-        verify(dbController, never()).addUserToDB(anyString(), anyString(), anyString(), anyString(), anyString());
+        verify(UserDao, never()).create(user);
     }
 
 
     //Проверяет, что при вводе некорректного номера телефона функция addUser возвращает ошибку INVALID_PHONE.
     @Test
-    void addUser_InvalidPhone_ShouldReturnInvalidPhoneError() {
-        // Arrange
-        String name = "Test User";
-        String email = "test@example.com";
-        String phone = "123456789"; // Invalid phone number
-        String plainPassword = "Password123";
-        String address = "Test Address";
+    void testAddUserInvalidPhone() {
+        User user= new User(3,"Test User","test23@example.com",
+                "invalid-phone", "Pewrvrvg#d135","Test Address");
 
         // Act
-        ArrayList<AddUserError> errors = userService.addUser(name, email, phone, plainPassword, address);
+        ArrayList<AddUserError> errors = userService.addUser(user);
 
-        // Assert
-        assertEquals(1, errors.size());
+        //assertEquals(1, errors.size());
         assertTrue(errors.contains(PhoneAndEmailError.INVALID_PHONE));
-        verify(dbController, never()).addUserToDB(anyString(), anyString(), anyString(), anyString(), anyString());
+        verify(UserDao, never()).create(user);
     }
 
     //Проверяет, что при вводе некорректного пароля функция addUser возвращает соответствующие ошибки валидации пароля.
     @SuppressWarnings("checkstyle:MethodName")
     @Test
-    void addUserInvalidPasswordShouldReturnPasswordErrors() {
-        // Arrange
-        String name = "Test User";
-        String email = "test@example.com";
-        String phone = "1234567890";
-        String plainPassword = "Pa#24"; // Invalid password (too short)
-        String address = "Test Address";
+    void testAddUserInvalidPassword() {
+        User user= new User(4,"Test User","test23@example.com",
+                "1234567890", "pas123","Test Address");
 
-        // Act
-        ArrayList<AddUserError> errors = userService.addUser(name, email, phone, plainPassword, address);
+        ArrayList<AddUserError> errors = userService.addUser(user);
 
-        // Assert
-        assertEquals(1, errors.size());
+        //assertEquals(1, errors.size());
         assertTrue(errors.contains(PasswordErrorType.TOO_SHORT));
-        verify(dbController, never()).addUserToDB(anyString(), anyString(), anyString(), anyString(), anyString());
+        verify(UserDao, never()).create(user);
     }
 
     //Проверяет, что при попытке добавить пользователя с уже существующим адресом электронной почты функция addUser возвращает ошибку EMAIL_ALREADY_EXISTS.
     @Test
-    void addUserEmailAlreadyExistsShouldReturnEmailAlreadyExistsError() {
-        // Arrange
-        String name = "Test User";
-        String email = "test@example.com";
-        String phone = "1234567890";
-        String plainPassword = "Password123";
-        String address = "Test Address";
+    void testAddUserEmailAlreadyExists() {
+        User user= new User(5,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");
 
-        //when(dbController.addUserToDB(name, email, phone, anyString(), address)).thenReturn(false);
+        ArrayList<AddUserError> errors = userService.addUser(user);
 
-        // Act
-        ArrayList<AddUserError> errors = userService.addUser(name, email, phone, plainPassword, address);
-
-        // Assert
-        //assertEquals(1, errors.size());
         assertTrue(errors.contains(PhoneAndEmailError.EMAIL_ALREADY_EXISTS));
-        verify(dbController).addUserToDB(name, email, phone, anyString(), address);
+        verify(UserDao, never()).create(user);
     }
 
     //Проверяет, что при вводе корректных учетных данных функция authenticateUser возвращает true.
     @Test
-    void authenticateUser_ValidCredentials_ShouldReturnTrue() {
-        // Arrange
-        String email = "test@example.com";
-        String plainPassword = "Password123";
+    void testAuthenticateUserSuccess() {
+        String email = "test23@example.com";
+        String plainPassword = "Pewrvrvg#d135";
+        String hashedPassword = PasswordUtils.hashPassword(plainPassword);
+
+        when(dbController.getUserPasswordByEmail(email)).thenReturn(hashedPassword);
+        // Act
+        boolean success = userService.authenticateUser(email, plainPassword);
+        // Assert
+        assertTrue(success);
+        verify(dbController).getUserPasswordByEmail(email);
+    }
+
+    //Проверяет, что authenticateUser возвращает false, если введенный пароль не совпадает с хэшированным паролем, хранящимся в базе данных.
+    @Test
+    void testAuthenticateUserInvalidPassword() {
+        String email = "test23@example.com";
+        String plainPassword = "Pewrvrvg#d135";
         String hashedPassword = PasswordUtils.hashPassword(plainPassword);
 
         when(dbController.getUserPasswordByEmail(email)).thenReturn(hashedPassword);
@@ -139,147 +127,123 @@ class UserServiceTest {
         boolean success = userService.authenticateUser(email, plainPassword);
 
         // Assert
-        assertTrue(success);
-        verify(dbController).getUserPasswordByEmail(email);
-    }
-
-    //Проверяет, что при вводе некорректного адреса электронной почты функция updateUser возвращает ошибку
-    @Test
-    void authenticateUser_InvalidEmail_ShouldReturnFalse() {
-        // Arrange
-        String email = "test@example.com";
-        String plainPassword = "Password123";
-        when(dbController.getUserPasswordByEmail(email)).thenReturn(null);
-
-        // Act
-        boolean success = userService.authenticateUser(email, plainPassword);
-
-        // Assert
         assertFalse(success);
         verify(dbController).getUserPasswordByEmail(email);
+
+
+        /*when(userDao.getUserPasswordByEmail(email)).thenReturn(hashedPassword);
+        when(PasswordUtils.checkPassword(plainPassword, hashedPassword)).thenReturn(false);
+
+        boolean isAuthenticated = userService.authenticateUser(email, plainPassword);
+
+        assertFalse(isAuthenticated);
+        verify(userDao, times(1)).getUserPasswordByEmail(email);
+        verify(PasswordUtils, times(1)).checkPassword(plainPassword, hashedPassword);*/
     }
 
-    //Проверяет, что authenticateUser возвращает false, если введенный пароль не совпадает с хэшированным паролем, хранящимся в базе данных.
+
+    //Проверяет аутентификацию несуществующего пользователя.
     @Test
-    void authenticateUser_InvalidPassword_ShouldReturnFalse() {
-        // Arrange
-        String email = "test@example.com";
-        String plainPassword = "Password123";
-        String hashedPassword = PasswordUtils.hashPassword("WrongPassword");
+    void testAuthenticateUserNotFound() {
+        String email = "john.doe@example.com";
+        String plainPassword = "password";
+        when(UserDao.getUserPasswordByEmail(email)).thenReturn(null);
 
-        when(dbController.getUserPasswordByEmail(email)).thenReturn(hashedPassword);
+        boolean isAuthenticated = userService.authenticateUser(email, plainPassword);
 
-        // Act
-        boolean success = userService.authenticateUser(email, plainPassword);
-
-        // Assert
-        assertFalse(success);
-        verify(dbController).getUserPasswordByEmail(email);
+        assertFalse(isAuthenticated);
+        verify(UserDao, times(1)).getUserPasswordByEmail(email);
+        verify(PasswordUtils, never()).checkPassword(plainPassword, anyString());
     }
+
 
     //Проверить, что updateUser не возвращает ошибок, если переданы валидные данные пользователя.
     @Test
-    void updateUser_ValidData_ShouldReturnEmptyErrors() {
-        // Arrange
-        int id = 1;
-        String name = "Updated User";
-        String email = "updated@example.com";
-        String phone = "+375255125747";
-        String plainPassword = "NewSomething14;";
-        String address = "Updated Address";
+    void testUpdateUserValidData() {
+        User user= new User(1,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");
 
         // Act
-        ArrayList<AddUserError> errors = userService.updateUser(id, name, email, phone, plainPassword, address);
+        ArrayList<AddUserError> errors = userService.updateUser(user);
 
         // Assert
         assertTrue(errors.isEmpty());
-        verify(dbController).updateUserInDB(id, name, email, phone, anyString(), address);
+        //verify(dbController).updateUserInDB(user);
     }
 
     //Проверить, что updateUser возвращает ошибку INVALID_EMAIL, если передан некорректный email.
     @Test
-    void updateUser_InvalidEmail_ShouldReturnInvalidEmailError() {
-        // Arrange
-        int id = 1;
-        String name = "Updated User";
-        String email = "invalid.email";
-        String phone = "9876543210";
-        String plainPassword = "NewPassword";
-        String address = "Updated Address";
+    void testUpdateUserInvalidEmail() {
+        User user= new User(1,"Test User","invalid-email",
+                "1234567890", "Pewrvrvg#d135","Test Address");
+        ArrayList<AddUserError> errors = userService.updateUser(user);
 
-        // Act
-        ArrayList<AddUserError> errors = userService.updateUser(id, name, email, phone, plainPassword, address);
-
-        // Assert
-        //assertEquals(1, errors.size());
+        assertEquals(1, errors.size());
         assertTrue(errors.contains(PhoneAndEmailError.INVALID_EMAIL));
-        verify(dbController, never()).updateUserInDB(anyInt(), anyString(), anyString(), anyString(), anyString(), anyString());
-    }
+        verify(UserDao, never()).update(user);
+        }
 
     //Проверить, что updateUser возвращает ошибку INVALID_PHONE, если передан некорректный номер телефона
     @Test
-    void updateUser_InvalidPhone_ShouldReturnInvalidPhoneError() {
-        // Arrange
-        int id = 1;
-        String name = "Updated User";
-        String email = "updated@example.com";
-        String phone = "987654321"; // Invalid phone number
-        String plainPassword = "NewPassword";
-        String address = "Updated Address";
+    void testUpdateUserInvalidPhone() {
+        User user= new User(1,"Test User","test23@example.com",
+                "invalid-phone", "Pewrvrvg#d135","Test Address");
+        ArrayList<AddUserError> errors = userService.updateUser(user);
 
-        // Act
-        ArrayList<AddUserError> errors = userService.updateUser(id, name, email, phone, plainPassword, address);
-
-        // Assert
         assertEquals(1, errors.size());
         assertTrue(errors.contains(PhoneAndEmailError.INVALID_PHONE));
-        verify(dbController, never()).updateUserInDB(anyInt(), anyString(), anyString(), anyString(), anyString(), anyString());
+        verify(UserDao, never()).update(user);
     }
 
     //Проверить, что updateUser возвращает ошибки, связанные с паролем, если передан некорректный пароль.
     @Test
-    void updateUser_InvalidPassword_ShouldReturnPasswordErrors() {
-        // Arrange
-        int id = 1;
-        String name = "Updated User";
-        String email = "updated@example.com";
-        String phone = "9876543210";
-        String plainPassword = "Pa#3"; // Invalid password (too short)
-        String address = "Updated Address";
+    void testUpdateUserInvalidPassword() {
+        User user= new User(1,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");
 
-        // Act
-        ArrayList<AddUserError> errors = userService.updateUser(id, name, email, phone, plainPassword, address);
+        ArrayList<AddUserError> errors = userService.updateUser(user);
 
-        // Assert
         assertEquals(1, errors.size());
         assertTrue(errors.contains(PasswordErrorType.TOO_SHORT));
-        verify(dbController, never()).updateUserInDB(anyInt(), anyString(), anyString(), anyString(), anyString(), anyString());
+        verify(UserDao, never()).update(user);
     }
 
     // Проверяет, что функция deleteUser удаляет пользователя.
     @Test
-    void deleteUser_ShouldCallDeleteUserFromDB() {
-        // Arrange
-        int userId = 1;
+    void testDeleteUserSuccess() {
+        User user= new User(1,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");
 
-        // Act
-        userService.deleteUser(userId);
-
+        userService.deleteUser(user);
         // Assert
-        verify(dbController).deleteUserFromDB(userId);
+        boolean result = userService.deleteUser(user);
+        assertTrue(result);
+        verify(UserDao, times(1)).delete(user);
+    }
+
+    //Проверяет неудачное удаление пользователя.
+    @Test
+    void testDeleteUserFailed() {
+        User user= new User(1,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");
+
+        userService.deleteUser(user);
+        // Assert
+        boolean result = userService.deleteUser(user);
+        assertTrue(result);
+        verify(UserDao, times(1)).delete(user);
     }
 
     //Проверяет, что функция getUserById возвращает пользователя по его идентификатору.
     @Test
-    void getUserById_ShouldCallGetUserFromDB() {
+    void testGetUserById() {
         // Arrange
         int userId = 1;
-        User user = new User(userId, "Test User", "test@example.com", "1234567890", "Password123", "Test Address");
-
-        when(dbController.getUserFromDB(userId)).thenReturn(user);
+        User user= new User(1,"Test User","test23@example.com",
+                "1234567890", "Pewrvrvg#d135","Test Address");when(dbController.getUserFromDB(userId)).thenReturn(user);
 
         // Act
-        User returnedUser = userService.getUserById(userId);
+        User returnedUser = userService.getUserById(user);
 
         // Assert
         assertEquals(user, returnedUser);
@@ -288,20 +252,13 @@ class UserServiceTest {
 
     //Проверяет, что функция getAllUsers возвращает всех пользователей
     @Test
-    void getAllUsers_ShouldCallGetAllUsers() {
-        // Arrange
-        ArrayList<User> users = new ArrayList<>(Arrays.asList(
-                new User(1, "User 1", "user1@example.com", "1234567890", "Password123", "Address 1"),
-                new User(2, "User 2", "user2@example.com", "9876543210", "Password456", "Address 2")
-        ));
+    void testGetAllUsers() {
+        List<Object> users = new ArrayList<>();
+        when(UserService.getAllUsers()).thenReturn(users);
 
-        when(dbController.getAllUsers()).thenReturn(users);
+        List<Object> fetchedUsers = userService.getAllUsers();
 
-        // Act
-        ArrayList<User> returnedUsers = userService.getAllUsers();
-
-        // Assert
-        assertEquals(users, returnedUsers);
-        verify(dbController).getAllUsers();
+        assertEquals(users, fetchedUsers);
+        verify(userDao, times(1)).readAll();
     }
 }
