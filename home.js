818e96ff3804ddc62ea2d9415311a3b593c41c16
Darya2302 <119890590+Darya2302@users.noreply.github.com>
2024-11-27 17:38:08 +0300

    Create home.js

---
+++ home.js
@@ -0,0 +1,70 @@
+
+document.getElementById('find').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'find.html';
+});
+
+document.getElementById('lost').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'lost.html';
+});
+
+document.getElementById('firstpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('secondpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('thirdpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('fourpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('fivepetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('Profile').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'profile.html';
+});
+
+document.getElementById('Chat').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'chat.html';
+});
+
+document.getElementById('Filter').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'filter.html';
+});
+
+document.getElementById('My_listing').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'mylisting.html';
+});
+
+document.getElementById('Instruction').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'instruction.html';
+});
+document.getElementById('About_us').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'aboutus.html';
+});
+
+document.getElementById('Exit').addEventListener('click', function() {
+    event.preventDefault(); 
+    alert('Вы вышли из профиля');
+    window.location.href = 'homeguest.html';
+});
