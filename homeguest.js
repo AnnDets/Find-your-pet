8a6cdd13cb7077364a7823930c086dbcaf34c79a
Darya2302 <119890590+Darya2302@users.noreply.github.com>
2024-12-09 16:55:57 +0300

    Create homeguest.js

---
+++ homeguest.js
@@ -0,0 +1,64 @@
+
+document.getElementById('find').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'registration.html';
+});
+
+document.getElementById('lost').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'registration.html';
+});
+
+document.getElementById('firstpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('secondpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('thirdpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('fourpetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('fivepetButton').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'petpage.html';
+});
+
+document.getElementById('Registration').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'registration.html';
+});
+
+document.getElementById('Chat').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'chat.html';
+});
+
+document.getElementById('Filter').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'filter.html';
+});
+
+document.getElementById('Instruction').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'instruction.html';
+});
+document.getElementById('About_us').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'aboutus.html';
+});
+
+document.getElementById('Login').addEventListener('click', function() {
+    event.preventDefault(); 
+    window.location.href = 'login.html';
+});
