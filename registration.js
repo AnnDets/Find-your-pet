d8b5da9e7d171f31bd834eed3cd7ce33826314cc
sonico <115878416+sonico5206@users.noreply.github.com>
2024-12-15 21:02:25 +0300

    Update registration.js
    добавление выбора языка

--- registration.js
+++ registration.js
@@ -25,6 +25,82 @@ document.getElementById('register').addEventListener('click', function() {
         // Если все проверки пройдены, перенаправляем на домашнюю страницу
     window.location.href = 'home.html'; // Укажите адрес вашей домашней страницы
     }
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+    const langData = {
+        ru: {
+            registration: "Регистрация",
+            username: "Введите ваше имя:",
+            adress: "Введите ваш адрес:",
+            telephone: "Введите ваш телефон:",
+            email: "Введите ваш email:",
+            password: "Введите пароль:",
+            repassword: "Повторите пароль:",
+            mandatoryFields: "Поля со * являются обязательными",
+            agreement: "Я согласен с условиями",
+            register: "Зарегистрироваться",
+            errorMessage: "Ошибка: Пожалуйста, заполните все поля.",
+            errorMessage2: "Ошибка: Пароль не совпадает.",
+            errorMessage3: "Ошибка: Согласитесь с политикой конфиденциальности."
+        },
+        en: {
+                registration: "Registration",
+                username: "Enter your name:",
+                adress: "Enter your address:",
+                telephone: "Enter your phone:",
+                email: "Enter your email:",
+                password: "Enter password:",
+                repassword: "Repeat password:",
+                mandatoryFields: "Fields marked with * are mandatory",
+                agreement: "I agree to the terms",
+                register: "Register",
+                errorMessage: "Error: Please fill in all fields.",
+                errorMessage2: "Error: Password does not match.",
+                errorMessage3: "Error: Agree to the privacy policy."
+        }
+    };
+
+    const languageSwitcher = document.getElementById("languageSwitcher");
     
+    const updateLanguage = () => {
+        currentLang = document.getElementById("languageSwitcher").value;
+        console.log("Current language data:", currentLang);
+
+        
+        // Обновляем текстовые поля напрямую
+        document.getElementById("registration").textContent = langData[currentLang].registration;
+
+        const texts = [
+            langData[currentLang].username,
+            langData[currentLang].adress,
+            langData[currentLang].telephone,
+            langData[currentLang].email,
+            langData[currentLang].password,
+            langData[currentLang].repassword
+        ];
+
+        const styledTexts = document.querySelectorAll(".styled-text");
+        styledTexts.forEach((element, index) => {
+        element.textContent = texts[index];
+        });
+        //document.querySelector(".styled-redtext").textContent = langData[currentLang].mandatoryFields;
+        document.getElementById("agreement").nextSibling.textContent = langData[currentLang].agreement;
+        document.getElementById("register").textContent = langData[currentLang].register;
+
+        const errors=[
+            langData[currentLang].errorMessage,
+            langData[currentLang].errorMessage2,
+            langData[currentLang].errorMessage3];
+
+        const errorTexts = document.querySelectorAll(".error");
+        errorTexts.forEach((element, index) => {
+        element.textContent = errors[index];
+        });
+    };
+    // Слушатель изменения языка
+    languageSwitcher.addEventListener("change", updateLanguage);
+
+    // Инициализация
+    updateLanguage();
 });
