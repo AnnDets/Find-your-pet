eb02b9d536d15387b406852ea8e1006a648e9d4b
sonico <115878416+sonico5206@users.noreply.github.com>
2024-12-16 13:39:50 +0300

    Update login.js
    languageSwitcher

--- login.js
+++ login.js
@@ -22,3 +22,57 @@ document.getElementById('login').addEventListener('click', function() {
     };
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const langData = {
+        ru: {
+            title:"Вход",
+            email: "Введите ваш email:",
+            password: "Введите пароль:",
+            login: "Войти",
+            errorMessage: "Ошибка: Пожалуйста, заполните все поля."
+        },
+        en: {
+            title:"Login",
+            email: "Enter your email:",
+            password: "Enter your password:",
+            login: "Login",
+            errorMessage: "Error: Please fill in all fields."
+        }
+    };
+    
+    const languageSwitcher = document.getElementById("languageSwitcher");
+    
+    const updateLanguage = () => {
+        currentLang = document.getElementById("languageSwitcher").value;
+        console.log("Current language data:", currentLang);
+    
+        // Переводим текстовые элементы
+        document.getElementById("title").textContent = langData[currentLang].title;
+
+        const texts = [
+            langData[currentLang].email,
+            langData[currentLang].password,
+            langData[currentLang].login
+        ];
+
+        const styledTexts = document.querySelectorAll(".styled-text");
+        styledTexts.forEach((element, index) => {
+        element.textContent = texts[index];
+
+        const errors=[
+            langData[currentLang].errorMessage];
+
+        const errorTexts = document.querySelectorAll(".error");
+        errorTexts.forEach((element, index) => {
+        element.textContent = errors[index];
+        });
+    });
+};
+    
+// Слушатель изменения языка
+languageSwitcher.addEventListener("change", updateLanguage);
+
+// Инициализация
+updateLanguage();
+});
